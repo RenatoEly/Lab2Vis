54eeb60411405200181fe008e8ab470d7c8d0d3c
Renato Ely <ely.reds@gmail.com>
2016-12-22 13:41:15 +0000

    Testando d3

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 /* eslint-env node */
+main(){
+    var clickObj = document.getElementById('click');
+    clickObj.onClick = function(){
+        if (this.style.backgroundColor){
+            this.style.backgroundColor = '';
+        } else{
+            this.style.backgroundColor = 'red';
+        }
+    }
+    
+    d3.selectAll('.hover-me')
+       .on('mouseover', function(){
+           this.style.backgroundColor = 'blue';
+       })
+       .on('mouseleave', function(){
+           this.styel.backgroundColor = '';
+       })
+}
